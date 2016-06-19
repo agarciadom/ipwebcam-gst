b2e8dca73be9b59e3461455ffe3e586f43a620cb
Laptander <dev-laptander@yandex.ru>
2016-06-19 14:07:50 +0300

    add troubleshooting for Arch Linux

--- prepare-videochat.sh
+++ prepare-videochat.sh
@@ -32,7 +32,7 @@
 # INSTALLATION
 #
 # In Arch Linux
-# install ipwebcam-gst package from AUR
+# install ipwebcam-gst-git package from AUR
 #
 # TROUBLESHOOTING
 #
@@ -59,11 +59,18 @@
 #
 # To make sure the video from IP Webcam works for you (except for
 # v4l2loopback and your video conference software), try this command
-# with a simplified pipeline:
+# with a simplified pipeline (do not forget to replace $IP and $PORT
+# with your values):
 #
+# on Debian:
 #   gst-launch souphttpsrc location="http://$IP:$PORT/videofeed" \
 #               do-timestamp=true is-live=true \
 #    ! multipartdemux ! jpegdec ! ffmpegcolorspace ! ximagesink
+#
+# on Arch Linux:
+#   gst-launch-1.0 souphttpsrc location="http://$IP:$PORT/videofeed" \
+#               do-timestamp=true is-live=true \
+#    ! multipartdemux ! jpegdec ! videoconvert ! ximagesink
 #
 # You should be able to see the picture from your webcam on a new window.
 # If that doesn't work, there's something wrong with your connection to
